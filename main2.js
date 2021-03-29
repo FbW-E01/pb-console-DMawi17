55dcc1a7d12fa4eda175116899f131f93a3d7cf5
Dagmawi <asegda17@gmail.com>
2021-03-29 13:45:25 +0200

    intro-js

---
+++ main2.js
@@ -0,0 +1,21 @@
+console.log('this is an exercise in console logging');
+
+const consoleValue = 'This is the value.'
+console.log(consoleValue);
+
+// const firstName = 'DMawi';
+// const lastName = 'Asegid';
+// console.log(firstName,lastName);
+
+const firstName = 'John';
+const lastName = 'Doe';
+const city = 'Munich';
+const occupation = 'Programmer';
+const company = 'DCI';
+console.log(firstName,lastName,occupation,city,company);
+
+const profile = (firstName + ' ' + lastName + ' lives in ' + city + ' where he works as a ' + occupation + ' ' + 'for ' + company)
+
+// const profile = `${firstName} ${lastName} lives in ${city} where he works as a ${occupation} for ${company}.`;
+
+console.log(profile);
